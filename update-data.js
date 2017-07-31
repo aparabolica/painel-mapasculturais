e33716c4a7e68db059306ff7e7fdba5e2fd5c3f5
Vitor George <vitor.george@aparabolica.com.br>
2017-07-31 10:49:50 -0300

    Normalize place names and create questions for territories

--- update-data.js
+++ update-data.js
@@ -1,4 +1,5 @@
 const replay = require("replay");
+const _ = require("lodash");
 var request = require("request").defaults({ jar: true });
 const async = require("async");
 const elementTypes = ["agent", "space", "project", "event"];
@@ -130,11 +131,65 @@ function fetchTypeData(install, type, doneFetchElementType) {
 }
 
 /*
+ * Normalize city names
+ */
+const altName = require("./altnames.json");
+function normalizeName(type, input) {
+  const names = altName[type];
+
+  if (input) {
+    // Remove leading/trailing whitespaces
+    input = input.trim();
+
+    // Replaces multiple spaces with one space
+    input = input.replace(/  +/g, " ");
+  }
+
+  // Do not process null or empty values
+  if (!input || input == "") return null;
+
+  // Name is correct, return unchanged
+  if (names[input]) return input;
+
+  // Try to find an alternative name
+  const loweredInput = input.toLowerCase();
+  for (name of Object.keys(names)) {
+    if (
+      name.toLowerCase() == loweredInput ||
+      _.includes(names[name], loweredInput)
+    )
+      return name;
+  }
+
+  // Name don't need to be changed
+  return input;
+}
+
+/*
+ * Prepare data
+ */
+function preprocessData(install, type, data) {
+  return _.map(data, function(d) {
+    d.createdAt = new Date(d.createTimestamp.date);
+    d.updatedAt = d.updateTimestamp ? new Date(d.updateTimestamp.date) : null;
+    d.city = normalizeName("city", d["En_Municipio"] || d["geoMunicipio"]);
+    d.district = normalizeName(
+      `district_${install.name}`,
+      d["geoDistrito"] || d["En_Bairro"]
+    );
+
+    return d;
+  });
+}
+
+/*
  * Import data
  */
 
 function importData(install, type, data, doneImportData) {
   console.log(`Importing ${type}s...`);
+
+  data = preprocessData(install, type, data);
 
   var collection = dbConnection.collection(`${install.name}-${type}s`);
 
@@ -152,9 +207,31 @@ function importData(install, type, data, doneImportData) {
       );
     },
     function(err) {
-      if (err) console.log(err.message);
+      if (err) doneImportData(err);
       else console.log(`Imported ${data.length} ${type}s.`);
-      doneImportData(err);
+      postprocessData(install, doneImportData);
     }
   );
+}
+
+/*
+ * Post-process data
+ */
+function postprocessData(install, donePostprocessData) {
+  console.log(`Post-processing ${install.name}...`);
+
+  dbConnection.collection(`${install.name}-agents`).aggregate([
+    { $match: {} },
+    {
+      $group: {
+        _id: "$userId",
+        createdAt: { $min: "$createdAt" },
+        agents: { $push: "$id" },
+        agentCount: { $sum: 1 }
+      }
+    },
+    { $out: `${install.name}-users` }
+  ], function(err) {
+    donePostprocessData(err);
+  });
 }
